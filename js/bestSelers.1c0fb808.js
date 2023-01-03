8cd623c889010891ae3dff96a4c0d01721f90405
hamidGit68 <hadavudi@gmail.com>
2023-01-03 19:32:45 +0330

    initial commit of vue-netlify

---
+++ js/bestSelers.1c0fb808.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["bestSelers"],{"2f8a":function(e,t,s){"use strict";s.r(t);var n=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},r=[function(){var e=this,t=e.$createElement,s=e._self._c||t;return s("div",{staticClass:"selers"},[s("h1",[e._v("صفحه پرفروش ترین ها")])])}],a={name:"BestSelers"},c=a,l=s("2877"),i=Object(l["a"])(c,n,r,!1,null,"c5579a04",null);t["default"]=i.exports}}]);
+//# sourceMappingURL=bestSelers.1c0fb808.js.map
---
+++ js/translateServ.d8d4c336.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["translateServ"],{"41e2":function(e,t,n){"use strict";n.r(t);var a=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},r=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"trans"},[n("h1",[e._v("صفحه ترجمه کتاب")])])}],s={name:"TranslateServ"},c=s,l=n("2877"),i=Object(l["a"])(c,a,r,!1,null,"76e3df25",null);t["default"]=i.exports}}]);
+//# sourceMappingURL=translateServ.d8d4c336.js.map
